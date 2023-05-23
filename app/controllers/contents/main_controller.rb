f9b1790cab0182158e32a5a77ab7be06a978957e
Aleksandr <aleksvarlaam@gmail.com>
2023-05-23 12:03:04 +0500

    Added page about demo

---
+++ app/controllers/contents/main_controller.rb
@@ -0,0 +1,9 @@
+module Contents
+  class MainController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
-    root "main#index"
+    get "/about", to: "contents/main#about", as: 'about'
+    # Root
+    root "contents/main#index"
   end
 end
