7af1c4b11dc6749bac21e574e1b271475e7b14d4
Aleksandr <aleksvarlaam@gmail.com>
2023-05-26 18:04:20 +0500

    Added dark-mode|light-mode

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -4,7 +4,7 @@ module NavigationHelper
   def nav_tab(title, url, options = {})
     current_page = options.delete :current_page
 
-    css_class = current_page == title ? 'text-gray-400' : 'text-gray-200'
+    css_class = current_page == title ? 'text-gray-700 dark:text-gray-400' : 'text-gray-500 dark:text-gray-200'
 
     options[:class] = if options[:class]
                         "#{options[:class]} #{css_class}"
